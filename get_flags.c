6f4b89866bb589ae0e164af1a6fe9592ab796bb8
daniela23ai <degeonu23@gmail.com>
2023-10-17 06:13:58 -0700

    get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * get_flags - Calculates active flags
+ * to_flags - To calculates an active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
-int get_flags(const char *format, int *i)
+int to_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
